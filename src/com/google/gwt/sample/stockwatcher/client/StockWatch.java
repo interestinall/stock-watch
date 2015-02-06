546b67d74a1c193a4d44e43eeec160097d0efec7
Justin LeFurjah <lefurjah@gmail.com>
2015-02-06 14:51:00 +0100

    basic ui elements added to page

--- src/com/google/gwt/sample/stockwatcher/client/StockWatch.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatch.java
@@ -5,12 +5,16 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 	public class StockWatch implements EntryPoint{
 
+		/**
+		 ** Layout Objects 
+		 **/
 		private VerticalPanel mainPanel = new VerticalPanel();
 		private FlexTable stockTable = new FlexTable();
 		private HorizontalPanel addPanel = new HorizontalPanel();
@@ -19,16 +23,36 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 		private Label lastUpdatedLabel = new Label();
 		
 		/**
-		 *  ENTRY POINT
-		 */
+		 **   ENTRY POINT
+		 **/
 		
 		
 		public void onModuleLoad() {
 			 // TODO Create table for stock data.
+			
+			stockTable.setText(0, 0, "Symbol");
+			stockTable.setText(0, 1, "Price");
+			stockTable.setText(0, 2, "Change");
+			stockTable.setText(0, 3, "remove");
+			
 		    // TODO Assemble Add Stock panel.
+			
+			addPanel.add(newSymbolBox);
+			addPanel.add(addStockButton);
+			
 		    // TODO Assemble Main panel.
+			
+			mainPanel.add(stockTable);
+			mainPanel.add(addPanel);
+			mainPanel.add(lastUpdatedLabel);
+			
 		    // TODO Associate the Main panel with the HTML host page.
-		    // TODO Move cursor focus to the input box.
+		   
+			RootPanel.get("stocklist").add(mainPanel);
+			
+			// TODO Move cursor focus to the input box.
+			
+			newSymbolBox.setFocus(true);
 		}
 		
 		
